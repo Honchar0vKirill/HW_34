c48891800398684a4a6ebffc1f5c21061237e258
Hocnchar0vKirill <kirill2021honc@gmail.com>
2023-03-19 14:02:10 +0200

    ...

---
+++ AGE/script.js
@@ -0,0 +1,2 @@
+let textfield = prompt("У якому році ви народились?");
+alert(2023 - textfield - 1);
---
+++ QUIZ/script.js
@@ -0,0 +1,15 @@
+let question = confirm("Крим Український?");
+alert(question);
+if (question == true){
+    alert('Бандера одобрює')
+}
+else{
+    alert('ти москаль')
+}
+let question2 = prompt("У якому році Україна здобула незалежність?");
+if (question2 == 1991){
+    alert("Правильно")
+}
+else {
+    alert("Ти точно не москаль?")
+}
